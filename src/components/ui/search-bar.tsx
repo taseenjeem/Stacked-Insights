67db6018d465c075a31050fb1ee30481c71a60af
taseenjeem <taseenjeem.dev@gmail.com>
2025-01-23 22:18:09 +0600

    Update search-bar.tsx

--- src/components/ui/search-bar.tsx
+++ src/components/ui/search-bar.tsx
@@ -5,7 +5,11 @@ import { Search } from "lucide-react";
 export default function SearchBar() {
   return (
     <div className="flex items-center gap-2">
-      <Input type="text" className="w-full" placeholder="Search..." />
+      <Input
+        type="text"
+        className="w-full focus-visible:ring-0"
+        placeholder="Search..."
+      />
       <Button variant={"outline"}>
         <Search className="size-6" /> Search
       </Button>

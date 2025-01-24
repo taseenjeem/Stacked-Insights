46b9fe9a58dc12db223bb3cd91a33db033c70e27
taseenjeem <taseenjeem.dev@gmail.com>
2025-01-25 01:58:56 +0600

    event done

--- src/components/pages/landing-page/Events.tsx
+++ src/components/pages/landing-page/Events.tsx
@@ -1,3 +1,15 @@
+import EventCard from "@/components/ui/event-card";
+
 export default function Events() {
-  return <aside className="min-h-screen w-1/5">Hello World</aside>;
+  return (
+    <aside className="min-h-screen w-1/5 space-y-3">
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+    </aside>
+  );
 }
---
+++ src/components/ui/event-card.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { Card, CardHeader, CardTitle } from "./card";
+import thumbnail from "/public/images/react19.png";
+import Link from "next/link";
+import { Badge } from "./badge";
+
+export default function EventCard() {
+  return (
+    <Card>
+      <CardHeader className="space-y-2">
+        <div className="aspect-w-16 aspect-h-9 relative rounded-lg">
+          <Image
+            quality={50}
+            src={thumbnail}
+            alt="thumbnail image"
+            className="rounded-md object-cover object-center"
+            fill
+          />
+        </div>
+        <CardTitle>
+          <Link
+            href={``}
+            className="text-lg leading-none duration-300 hover:text-muted-foreground"
+          >
+            React 19: Unveiling the Game-Changing Features for Developers
+          </Link>
+        </CardTitle>
+        <div className="flex flex-wrap gap-2">
+          <Badge variant={"destructive"}>Event</Badge>
+          <Badge variant={"destructive"}>Registration On Going</Badge>
+        </div>
+      </CardHeader>
+    </Card>
+  );
+}

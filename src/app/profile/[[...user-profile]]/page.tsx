c424581fd25c5e0964fe48ad2e5ae96927d35e49
taseenjeem <taseenjeem.dev@gmail.com>
2025-01-30 02:02:20 +0600

    Update page.tsx

--- src/app/profile/[[...user-profile]]/page.tsx
+++ src/app/profile/[[...user-profile]]/page.tsx
@@ -1,9 +1,32 @@
+"use client";
 import { UserProfile } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
-export default function page() {
+export default function ProfilePage() {
+  const { theme, systemTheme } = useTheme();
+  const [currentTheme, setCurrentTheme] = useState<"dark" | undefined>(
+    undefined,
+  );
+
+  useEffect(() => {
+    if (theme === "system") {
+      setCurrentTheme(systemTheme as undefined | "dark");
+    } else {
+      setCurrentTheme(theme as undefined | "dark");
+    }
+  }, [theme, systemTheme]);
+
+  if (!currentTheme) return null;
+
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <UserProfile />
+      <UserProfile
+        appearance={{
+          baseTheme: currentTheme === "dark" ? dark : undefined,
+        }}
+      />
     </div>
   );
 }

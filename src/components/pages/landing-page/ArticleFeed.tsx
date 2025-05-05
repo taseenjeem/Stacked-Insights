9a0dac8f11ce44e9f9fdc0c37109aa93214916ee
taseenjeem <taseenjeem.dev@gmail.com>
2025-05-06 00:59:11 +0600

    updated article feed

--- src/components/pages/landing-page/ArticleFeed.tsx
+++ src/components/pages/landing-page/ArticleFeed.tsx
@@ -1,9 +1,32 @@
+"use client";
 import ArticleCard from "@/components/ui/article-card";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function ArticleFeed() {
+  const [blogs, setBlogs] = useState("discover");
+
   return (
     <div className="min-h-screen w-full space-y-3 md:w-3/5">
-      <h1 className="text-2xl font-semibold">Article Feed</h1>
+      <div className="flex items-center gap-4">
+        <h1 className="text-2xl font-semibold">Article Feed</h1>
+        <div className="space-x-1">
+          <Button
+            size={"sm"}
+            variant={blogs === "discover" ? "default" : "outline"}
+            onClick={() => setBlogs("discover")}
+          >
+            Discover
+          </Button>
+          <Button
+            size={"sm"}
+            variant={blogs === "following" ? "default" : "outline"}
+            onClick={() => setBlogs("following")}
+          >
+            Following
+          </Button>
+        </div>
+      </div>
       <ArticleCard />
       <ArticleCard />
       <ArticleCard />

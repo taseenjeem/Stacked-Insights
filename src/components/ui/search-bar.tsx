894bb54045bc40cc1e7588f41f5ee8fcc4caa3e0
taseenjeem <taseenjeem.dev@gmail.com>
2025-05-09 02:34:38 +0600

    search bar updated

--- src/components/ui/search-bar.tsx
+++ src/components/ui/search-bar.tsx
@@ -4,15 +4,16 @@ import { Search } from "lucide-react";
 
 export default function SearchBar() {
   return (
-    <div className="flex items-center gap-2">
+    <form className="flex items-center gap-2">
       <Input
+        required
         type="text"
         className="w-full focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
         placeholder="Search..."
       />
-      <Button variant={"outline"}>
+      <Button type="submit" variant={"outline"}>
         <Search className="size-6" /> Search
       </Button>
-    </div>
+    </form>
   );
 }
